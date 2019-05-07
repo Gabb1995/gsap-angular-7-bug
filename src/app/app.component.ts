7dc7b97199b3ed56ef75eff032c744e34dae67e0
Gabriel Caruana <gabb1995@gmail.com>
2019-05-07 08:38:01 +0200

    bug replicate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TimelineLite, Elastic, CSSPlugin, TweenMax} from 'gsap/all';
+
+const gsapStuff = [CSSPlugin, TweenMax, TimelineLite]; // To prevent tree shaking
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gsap-try';
+
+  ngOnInit(): void {
+      const tl = new TimelineLite({
+          paused: true
+      });
+
+      tl.from('#angular-logo', 2, {
+          opacity: 2,
+          scale: 2,
+          ease: Elastic.easeOut
+      });
+
+      tl.play();
+  }
 }
